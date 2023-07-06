7eba46efb88438d0a32aa45cbbcfff044db3f4fe
Lambada10 <62511588+Lambada10@users.noreply.github.com>
2023-07-06 18:22:07 +0200

    BrowseScreen: Make lyrics selectable/copyable #7

--- app/src/main/java/pl/lambada/songsync/ui/screens/BrowseScreen.kt
+++ app/src/main/java/pl/lambada/songsync/ui/screens/BrowseScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
@@ -208,9 +209,11 @@ fun BrowseScreen(viewModel: MainViewModel) {
                             OutlinedCard(
                                 modifier = Modifier.padding(8.dp), shape = RoundedCornerShape(10.dp)
                             ) {
-                                Text(
-                                    text = lyricsResult, modifier = Modifier.padding(8.dp)
-                                )
+                                SelectionContainer {
+                                    Text(
+                                        text = lyricsResult, modifier = Modifier.padding(8.dp)
+                                    )
+                                }
                             }
 
                             Button(onClick = {
