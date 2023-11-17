2b3e3a5f933920d076254bb4cf8f2409e8b7d58a
Gabriel Fontán <gabilessto@gmail.com>
2023-11-18 00:50:02 +0100

    feat(DEV-Info): Info/help for creating the proxy server of issue #44

--- app/src/main/java/pl/lambada/songsync/util/networking/Ktor.kt
+++ app/src/main/java/pl/lambada/songsync/util/networking/Ktor.kt
@@ -5,7 +5,14 @@ import io.ktor.client.engine.cio.CIO
 import kotlinx.serialization.json.Json
 
 object Ktor {
-    val client = HttpClient(CIO) {
+    val client = HttpClient(CIO.create {
+        //Here goes all the Engine config
+        //TODO: Add proxy support
+//        proxy = ProxyConfig(
+//            type = Proxy.Type.SOCKS,
+//            sa = java.net.InetSocketAddress(3030)
+//        )
+    }) {
         //In case of adding plugins, add them here
     }
 
