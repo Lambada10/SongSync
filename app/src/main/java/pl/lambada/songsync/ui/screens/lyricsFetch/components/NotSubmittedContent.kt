3befe879bceed247958e118df0c3ede8a1deff36
nxoim <ghite@nxoim.com>
2024-09-02 21:21:33 +0200

    fix

--- app/src/main/java/pl/lambada/songsync/ui/screens/lyricsFetch/components/NotSubmittedContent.kt
+++ app/src/main/java/pl/lambada/songsync/ui/screens/lyricsFetch/components/NotSubmittedContent.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
@@ -23,7 +24,7 @@ fun NotSubmittedContent(
     onQueryArtistChange: (String) -> Unit,
     onGetLyricsRequest: () -> Unit
 ) {
-    Column {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
         Spacer(modifier = Modifier.height(16.dp))
         CommonTextField(
             value = querySong,
