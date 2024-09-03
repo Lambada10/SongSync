d5ce9c2fa5d710e35a2a21f31590d07f9f58cc0f
nxoim <ghite@nxoim.com>
2024-09-03 02:59:17 +0200

    forgor

--- app/src/main/java/pl/lambada/songsync/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/pl/lambada/songsync/ui/screens/home/HomeScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
@@ -110,7 +109,6 @@ fun HomeScreen(
                         animatedVisibilityScope = animatedVisibilityScope,
                         resizeMode = SharedTransitionScope.ResizeMode.RemeasureToBounds,
                     ),
-                    elevation = FloatingActionButtonDefaults.elevation(0.dp),
                     onClick = { navController.navigate(LyricsFetchScreen()) }
                 ) {
                     Icon(
