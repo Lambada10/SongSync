656b2d53ebcec401d1ad87a590b702df4e849693
Omak2r3a <86030102+kerollosy@users.noreply.github.com>
2024-10-03 19:45:07 +0300

    Add MusixmatchAPI class for searching and retrieving synced lyrics

---
+++ app/src/main/java/pl/lambada/songsync/data/remote/lyrics_providers/others/MusixmatchAPI.kt
@@ -0,0 +1,72 @@
+package pl.lambada.songsync.data.remote.lyrics_providers.others
+
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import pl.lambada.songsync.domain.model.SongInfo
+import pl.lambada.songsync.domain.model.lyrics_providers.others.MusixmatchLyricsResponse
+import pl.lambada.songsync.domain.model.lyrics_providers.others.MusixmatchSearchResponse
+import pl.lambada.songsync.util.EmptyQueryException
+import pl.lambada.songsync.util.networking.Ktor.client
+import pl.lambada.songsync.util.networking.Ktor.json
+import java.net.URLEncoder
+
+class MusixmatchAPI {
+    private val baseURL = "https://kerollosy.alwaysdata.net"
+
+    /**
+     * Searches for synced lyrics using the song name and artist name.
+     * @param query The SongInfo object with songName and artistName fields filled.
+     * @return Search result as a SongInfo object.
+     */
+    suspend fun getSongInfo(query: SongInfo): SongInfo? {
+        val search = withContext(Dispatchers.IO) {
+            URLEncoder.encode(
+                "${query.songName} ${query.artistName}",
+                Charsets.UTF_8.toString()
+            )
+        }
+
+        if (search == " ")
+            throw EmptyQueryException()
+
+        val response = client.get(
+            "$baseURL/search?q=$search"
+        )
+        val responseBody = response.bodyAsText(Charsets.UTF_8)
+
+        if (response.status.value !in 200..299 || responseBody == "[]")
+            return null
+
+        val json = json.decodeFromString<List<MusixmatchSearchResponse>>(responseBody)
+
+        val result = json[0]
+
+        return SongInfo(
+            songName = result.track_name,
+            artistName = result.artist_name,
+            songLink = result.track_share_url,
+            albumCoverLink = result.album_cover,
+            musixmatchID = result.commontrack_id,
+        )
+    }
+
+    /**
+     * Searches for synced lyrics using the song name and artist name.
+     * @param id The ID of the song from search results.
+     * @return The synced lyrics as a string.
+     */
+    suspend fun getSyncedLyrics(id: Long): String? {
+        val response = client.get(
+            "$baseURL/lyrics?id=$id"
+        )
+        val responseBody = response.bodyAsText(Charsets.UTF_8)
+
+        if (response.status.value !in 200..299)
+            return null
+
+        val json = json.decodeFromString<MusixmatchLyricsResponse>(responseBody)
+        return json.subtitle_body
+    }
+}
---
+++ app/src/main/java/pl/lambada/songsync/domain/model/lyrics_providers/others/Musixmatch.kt
@@ -0,0 +1,26 @@
+package pl.lambada.songsync.domain.model.lyrics_providers.others
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MusixmatchSearchResponse(
+    val commontrack_id: Long,
+    val track_name: String,
+    val artist_name: String,
+    val album_name: String,
+    val album_cover: String,
+    val release_date: String,
+    val track_length: Int,
+    val track_share_url: String,
+    val album_id: String,
+    val has_lyrics: Boolean
+)
+
+@Serializable
+data class MusixmatchLyricsResponse(
+    val subtitle_id: Long,
+    val subtitle_length: Int,
+    val subtitle_language: String,
+    val updated_time: String,
+    val subtitle_body: String,
+)
