7acf114262cf3e871b18362d14c1f0bb574df9b3
Lambada10 <62511588+Lambada10@users.noreply.github.com>
2025-05-14 22:04:08 +0200

    Hotfix release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         minSdk = 21
         //noinspection OldTargetApi
         targetSdk = 35
-        versionCode = 432
-        versionName = "4.3.2"
+        versionCode = 433
+        versionName = "4.3.3"
 
         vectorDrawables {
             useSupportLibrary = true
